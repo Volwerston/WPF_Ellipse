b47bb760ed17466d6beb30bfdb172fc7cb9f315c
Yuriy Stetskyi <yst@interlogic.com.ua>
2017-12-15 15:42:18 +0200

    Куьщмув щвв сдфііуі фтв іефкеув штеупкфештп вкфт-т-вкщз агтсешщтфдшен

--- WPF_Ellipse/WPF_Ellipse/MainViewModel.cs
+++ WPF_Ellipse/WPF_Ellipse/MainViewModel.cs
@@ -16,70 +16,73 @@ using WPF_Ellipse;
 
 namespace WPF_Hexagones
 {
-	public class MainViewModel : INotifyPropertyChanged
-	{
-		public ObservableCollection<Ellipse> Ellipses { get; set; }
-		private Ellipse CurrentEllipse { get; set; }
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Ellipse> Ellipses { get; set; }
+        private Ellipse CurrentEllipse { get; set; }
 
         private Point PrevPoint { get; set; }
-		private uint CountEdges { get; set; }
+        private uint CountEdges { get; set; }
 
-		private Color currentColor;
-		public Color CurrentColor
-		{
-			get
-			{
-				return currentColor;
-			}
-			set
-			{
-				currentColor = value;
-				OnPropertyChanged("CurrentColor");
-			}
-		}
+        private Color currentColor;
+        public Color CurrentColor
+        {
+            get
+            {
+                return currentColor;
+            }
+            set
+            {
+                currentColor = value;
+                OnPropertyChanged("CurrentColor");
+            }
+        }
 
-		//Painting
-		public ICommand DrawClick_Command { get; private set; }
-		public ICommand ApplyColor_Command { get; set; }
+        public ICommand DrawClick_Command { get; private set; }
+        public ICommand ApplyColor_Command { get; set; }
 
-		//File Menu
-		public ICommand ClearWindow_Command { get; private set; }
-		public ICommand OpenFile_Command { get; private set; }
-		public ICommand SaveFile_Command { get; private set; }
-		public ICommand CloseWindow_Command { get; private set; }
+        public ICommand ClearWindow_Command { get; private set; }
+        public ICommand OpenFile_Command { get; private set; }
+        public ICommand SaveFile_Command { get; private set; }
+        public ICommand CloseWindow_Command { get; private set; }
 
-		//Selecting and draging hexogones
-		public ICommand SelectHexagone_Command { get; private set; }
-		public ICommand Drag_Command { get; private set; }
-		private bool AllowDragging { get; set; }
-		private Point MousePosition { get; set; }
-		private Polygon SelectedHexagone { get; set; }
+        public ICommand SelectEllipse_Command { get; private set; }
+        public ICommand Drag_Command { get; private set; }
+        private bool AllowDragging { get; set; }
+        private Point MousePosition { get; set; }
 
-		public MainViewModel()
-		{
-            Ellipses = new ObservableCollection<Ellipse>();      
+        private Ellipse SelectedEllipse { get; set; }
 
-			CountEdges = 0;
-			CurrentColor = Colors.Red;
-			CurrentEllipse = new Ellipse();
+        public MainViewModel()
+        {
+            Ellipses = new ObservableCollection<Ellipse>();
+
+            CountEdges = 0;
+            CurrentColor = Colors.Red;
+            CurrentEllipse = new Ellipse();
 
 
             PrevPoint = new Point(-1, -1);
 
-			ClearWindow_Command = new RelayCommand(ClearWindow);
-			OpenFile_Command = new RelayCommand(OpenFile);
-			SaveFile_Command = new RelayCommand(SaveFile);
-			CloseWindow_Command = new RelayCommand(CloseWindow);
-			DrawClick_Command = new RelayCommand(DrawClick);
-			ApplyColor_Command = new RelayCommand(ApplyColor);
+            ClearWindow_Command = new RelayCommand(ClearWindow);
+            OpenFile_Command = new RelayCommand(OpenFile);
+            SaveFile_Command = new RelayCommand(SaveFile);
+            CloseWindow_Command = new RelayCommand(CloseWindow);
+            DrawClick_Command = new RelayCommand(DrawClick);
+            ApplyColor_Command = new RelayCommand(ApplyColor);
 
-			SelectHexagone_Command = new RelayCommand(SelectHexagone);
-			Drag_Command = new RelayCommand(Drag);
-		}
+            SelectEllipse_Command = new RelayCommand(SelectEllipse);
+            Drag_Command = new RelayCommand(Drag);
+        }
 
-		private void DrawClick(object obj)
-		{
-			Point mousePoint = Mouse.GetPosition((IInputElement)obj);
+        private void DrawClick(object obj)
+        {
+            if(MousePosition != default(Point))
+            {
+                return;
+            }
+
+            Point mousePoint = Mouse.GetPosition((IInputElement)obj);
 
             Point prev = PrevPoint;
 
@@ -87,9 +90,9 @@ namespace WPF_Hexagones
 
             ++CountEdges;
 
-			if(CountEdges==2)
-			{
-				ColorsWindow colorWin = new ColorsWindow(this);
+            if (CountEdges == 2)
+            {
+                ColorsWindow colorWin = new ColorsWindow(this);
 
                 double width = Math.Abs(prev.X - PrevPoint.X);
                 double height = Math.Abs(prev.Y - PrevPoint.Y);
@@ -103,57 +106,56 @@ namespace WPF_Hexagones
 
                 CurrentEllipse.Margin = new Thickness(left, top, 0, 0);
 
-                if (colorWin.ShowDialog()==true)
-				{
-					CurrentEllipse.Fill = new SolidColorBrush(CurrentColor);
-				}
+                if (colorWin.ShowDialog() == true)
+                {
+                    CurrentEllipse.Fill = new SolidColorBrush(CurrentColor);
+                }
 
-				CurrentEllipse.Name = String.Format("Ellipse_{0}", Ellipses.Count + 1);
-				Ellipses.Add(CurrentEllipse);
+                CurrentEllipse.Name = String.Format("Ellipse_{0}", Ellipses.Count + 1);
+                Ellipses.Add(CurrentEllipse);
 
-				CurrentEllipse = new Ellipse();
-				OnPropertyChanged("Ellipses");
-				CountEdges = 0;
-			}
+                CurrentEllipse = new Ellipse();
+                OnPropertyChanged("Ellipses");
+                CountEdges = 0;
+            }
+        }
 
-		}
+        private void ApplyColor(object obj)
+        {
+            ColorsWindow colorsWindow = (ColorsWindow)obj;
+            colorsWindow.DialogResult = true;
+            colorsWindow.Close();
+        }
 
-		private void ApplyColor(object obj)
-		{
-			ColorsWindow colorsWindow = (ColorsWindow)obj;
-			colorsWindow.DialogResult = true;
-			colorsWindow.Close();
-		}
+        private void ClearWindow(object obj)
+        {
+            Ellipses.Clear();
+            OnPropertyChanged("Ellipses");
+        }
 
-		//File Menu
-		private void ClearWindow(object obj)
-		{
-			Ellipses.Clear();
-			OnPropertyChanged("Ellipses");
-		}
+        private void OpenFile(object obj)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".xml";
+            openFileDialog.Filter = "XML documents (.xml)|*.xml";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                List<EllipseDTO> ellipses = new List<EllipseDTO>();
 
-		private void OpenFile(object obj)
-		{
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.DefaultExt = ".xml";
-			openFileDialog.Filter = "XML documents (.xml)|*.xml";
-			if (openFileDialog.ShowDialog() == true)
-			{
-				string fileName = openFileDialog.FileName;
-				List<EllipseDTO> ellipses = new List<EllipseDTO>();
+                XmlSerializer serializer = new XmlSerializer(typeof(List<EllipseDTO>));
 
-				XmlSerializer serializer = new XmlSerializer(typeof(List<EllipseDTO>));
+                using (XmlReader reader = XmlReader.Create(fileName))
+                {
+                    ellipses = (List<EllipseDTO>)serializer.Deserialize(reader);
+                }
 
-				using (XmlReader reader = XmlReader.Create(fileName))
-				{
-					ellipses = (List<EllipseDTO>)serializer.Deserialize(reader);
-				}
+                Ellipses.Clear();
 
-				Ellipses.Clear();
-
-				for(int i=0; i<ellipses.Count; ++i)
-				{
-					Ellipses.Add(new Ellipse() {
+                for (int i = 0; i < ellipses.Count; ++i)
+                {
+                    Ellipses.Add(new Ellipse()
+                    {
                         Name = String.Format("Ellipse_{0}", i + 1),
                         Stroke = Brushes.Black,
                         Fill = new SolidColorBrush(ellipses[i].Color),
@@ -161,25 +163,25 @@ namespace WPF_Hexagones
                         Width = ellipses[i].Width,
                         Height = ellipses[i].Height
                     });
-				}
+                }
 
-				OnPropertyChanged("Ellipses");
-			}
-		}
+                OnPropertyChanged("Ellipses");
+            }
+        }
 
-		private void SaveFile(object obj)
-		{
-			SaveFileDialog saveFileDialog = new SaveFileDialog();
-			saveFileDialog.DefaultExt = ".xml";
-			saveFileDialog.FileName = "New_shapes.xml";
-			saveFileDialog.Filter = "XML documents (.xml)|*.xml";
-			if (saveFileDialog.ShowDialog() == true)
-			{
-				string fileName = saveFileDialog.FileName;
-				List<EllipseDTO> ellipses = new List<EllipseDTO>();
+        private void SaveFile(object obj)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".xml";
+            saveFileDialog.FileName = "New_shapes.xml";
+            saveFileDialog.Filter = "XML documents (.xml)|*.xml";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string fileName = saveFileDialog.FileName;
+                List<EllipseDTO> ellipses = new List<EllipseDTO>();
 
-				foreach(var elem in Ellipses)
-				{
+                foreach (var elem in Ellipses)
+                {
                     ellipses.Add(new EllipseDTO()
                     {
                         CenterX = elem.Margin.Left,
@@ -188,62 +190,73 @@ namespace WPF_Hexagones
                         Height = elem.Height,
                         Width = elem.Width
                     });
-				}
+                }
 
-				using (Stream outputFile = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
-				{
-					XmlSerializer serializer = new XmlSerializer(typeof(List<EllipseDTO>));
-					serializer.Serialize(outputFile, ellipses);
-				}
-			}
-		}
+                using (Stream outputFile = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<EllipseDTO>));
+                    serializer.Serialize(outputFile, ellipses);
+                }
+            }
+        }
 
-		private void CloseWindow(object obj)
-		{
-			(obj as MainWindow).Close();
-		}
+        private void CloseWindow(object obj)
+        {
+            (obj as MainWindow).Close();
+        }
 
-		//Selecting and draging hexogones
-		private void SelectHexagone(object obj)
-		{
-			Polygon curHexagone = (obj as Polygon);
-			curHexagone.MouseDown += new MouseButtonEventHandler(Hexagone_MouseDown);
-			OnPropertyChanged("Hexagones");
-		}
 
-		private void Drag(object obj)
-		{
-			Canvas plane = (obj as Canvas);
-			plane.MouseMove += new MouseEventHandler(Canvas_MouseMove);
-			plane.MouseUp += new MouseButtonEventHandler(Canvas_MouseUp);
-		}
+        private void SelectEllipse(object obj)
+        {
+            Ellipse curEllipse = (obj as Ellipse);
 
-		//Events
-		void Hexagone_MouseDown(object sender, MouseButtonEventArgs e)
-		{
-			AllowDragging = true;
-			SelectedHexagone = sender as Polygon;
-			MousePosition = e.GetPosition(SelectedHexagone);
-		}
+            curEllipse.MouseDown += new MouseButtonEventHandler(Ellipse_MouseDown);
+            curEllipse.MouseUp += new MouseButtonEventHandler(Ellipse_MouseUp);
+            curEllipse.MouseMove += new MouseEventHandler(Ellipse_MouseMove);
 
-		void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
-		{
-			AllowDragging = false;
-		}
+        }
 
-		void Canvas_MouseMove(object sender, MouseEventArgs e)
-		{
-			if (AllowDragging)
-			{
-				Canvas.SetLeft(SelectedHexagone, e.GetPosition(sender as IInputElement).X - MousePosition.X);
-				Canvas.SetTop(SelectedHexagone, e.GetPosition(sender as IInputElement).Y - MousePosition.Y);
-			}
-		}
+        private void Drag(object obj)
+        {
+            Canvas plane = (obj as Canvas);
+        }
 
-		public event PropertyChangedEventHandler PropertyChanged;
-		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-	}
+        void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            AllowDragging = true;
+            SelectedEllipse = sender as Ellipse;
+            MousePosition = e.GetPosition(SelectedEllipse);
+            SelectedEllipse.CaptureMouse();
+        }
+
+        void Ellipse_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AllowDragging = false;
+            (sender as Ellipse).ReleaseMouseCapture();
+        }
+
+        void Ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if (AllowDragging)
+            //{
+            //    Canvas.SetLeft(SelectedEllipse, e.GetPosition(sender as IInputElement).X - MousePosition.X);
+            //    Canvas.SetTop(SelectedEllipse, e.GetPosition(sender as IInputElement).Y - MousePosition.Y);
+            //}
+
+            if (AllowDragging)
+            {
+                Point punto = e.GetPosition(sender as IInputElement);
+                int mouseX = (int)punto.X;
+                int mouseY = (int)punto.Y;
+                SelectedEllipse.SetValue(Canvas.LeftProperty, (double)mouseX - MousePosition.X); //set x
+                SelectedEllipse.SetValue(Canvas.TopProperty, (double)mouseY - MousePosition.Y); //set y
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
